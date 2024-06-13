839193bed2cb13ff3da9549fdf770522c1380b82
AnimeAdam <Adam.Szlamp@gmail.com>
2024-06-13 14:14:19 +0100

    Player animates while jumping.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRb2D;
+    private Animator playerAnim;
     [SerializeField] private float jumpingSpeed = 600f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb2D = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRb2D.AddForce(Vector2.up * jumpingSpeed);
+            playerAnim.SetBool("player_jumping", true);
+        }
+        if (playerRb2D.velocity.y < 0f)
+        {
+            playerAnim.SetBool("player_jumping", false);
         }
     }
 }
